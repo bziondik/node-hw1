b512a6990a346e3ef8795db2e177b7f5dcfea427
bziondik <bziondik@gmail.com>
2018-03-08 22:01:05 +0300

    Done hw2

--- src/index.js
+++ src/index.js
@@ -5,30 +5,62 @@ var argv = require('minimist')(process.argv.slice(2));
 const folders = argv['_'];
 const isDeleteFolder = argv.d;
 
-const copyToResultDir = (base, res) => {
-  const files = fs.readdirSync(base);
+const checkFolderExist = (folder) => {
+  return new Promise((resolve, reject) => {
+    fs.access(folder, fs.constants.R_OK, (err) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+};
 
-  files.forEach(item => {
-    let localBase = path.join(base, item);
-    let state = fs.statSync(localBase);
-    if (state.isDirectory()) {
-      copyToResultDir(localBase, res);
-    } else {
-      const firstLetter = item.charAt(0);
-      const extname = path.extname(item);
-      const dest = path.join(res, firstLetter, extname, item);
-      try {
-        fs.accessSync(path.join(res, firstLetter));
-      } catch (err) {
-        fs.mkdirSync(path.join(res, firstLetter));
+const createFolder = (folder) => {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(folder, (err) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(true);
       }
-      try {
-        fs.accessSync(path.join(res, firstLetter, extname));
-      } catch (err) {
-        fs.mkdirSync(path.join(res, firstLetter, extname));
+    });
+  });
+};
+
+const copyToResultDir = (base, res) => {
+  return new Promise((resolve, reject) => {
+    const files = fs.readdirSync(base);
+
+    const promiseArray = files.map(item => {
+      let localBase = path.join(base, item);
+      let state = fs.statSync(localBase);
+      if (state.isDirectory()) {
+        return copyToResultDir(localBase, res);
+      } else {
+        const firstLetter = item.charAt(0);
+        const extname = path.extname(item);
+        const dest = path.join(res, firstLetter, extname, item);
+        return checkFolderExist(path.join(res, firstLetter))
+          .then((is) => {
+            if (!is) {
+              return createFolder(path.join(res, firstLetter));
+            }
+          })
+          .then(() => checkFolderExist(path.join(res, firstLetter, extname)))
+          .then((is) => {
+            if (!is) {
+              return createFolder(path.join(res, firstLetter, extname));
+            }
+          })
+          .then(() => {
+            fs.copyFileSync(localBase, dest);
+          });
       }
-      fs.copyFileSync(localBase, dest);
-    }
+    });
+
+    Promise.all(promiseArray).then(resolve);
   });
 };
 
@@ -50,17 +82,24 @@ const removeDir = (base) => {
   }
 };
 
-if (folders.length === 2) {
+const mainFunction = async (folders) => {
   try {
-    fs.accessSync(folders[0], fs.constants.R_OK);
-    fs.mkdirSync(folders[1]);
-    copyToResultDir(folders[0], folders[1]);
-    if (isDeleteFolder) {
-      removeDir(folders[0]);
+    const isFolderExist = await checkFolderExist(folders[0]);
+    const isFolderCreated = await createFolder(folders[1]);
+    console.log('isFolderExist ', isFolderExist, 'isFolderCreated ', isFolderCreated);
+    if (isFolderExist && isFolderCreated) {
+      await copyToResultDir(folders[0], folders[1]);
+      if (isDeleteFolder) {
+        removeDir(folders[0]);
+      }
     }
   } catch (err) {
     console.error(err.message);
   }
+};
+
+if (folders.length === 2) {
+  mainFunction(folders);
 } else {
   console.log('Enter path to folder and path to result folder!');
 }
